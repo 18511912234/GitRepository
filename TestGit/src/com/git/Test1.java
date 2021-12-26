d617bfc3666f42a3325d003812df38cd2bea831d
wangfengwei <w18511912234@163.com>
2021-12-26 22:07:24 +0800

    添加了Test1方法

---
+++ TestGit/src/com/git/Test1.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * @Auther:wangfengwei
+ * @Date:2021/12/26-12-Test1-下午10:06
+ */
+public class Test1 {
+    String name;
+
+    public Test1(String name) {
+        this.name = name;
+    }
+}
